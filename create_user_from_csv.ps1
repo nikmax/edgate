778e3550a7af23e5983913efa95456528f4f2427
nikmax <mail@antonsander.de>
2023-10-31 08:38:10 +0100

    einloggen mit global session, suche in mails, ausgabe postfächer über Arraysgit add create_user_from_csv.ps1

--- create_user_from_csv.ps1
+++ create_user_from_csv.ps1
@@ -1,69 +1,4 @@
 
-cls
-Add-Type -AssemblyName System.Web
-
-$logfile = "create_user_log.log"
-
-$url="edga.ncc.eurodata.de"
-$script="/cgi-bin/index.pl"
-
-
-$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-$cookie = New-Object System.Net.Cookie
-
-User-Login
-
-
-# anlegen # $cmd="?rm=insert_person"
-          # $params="&login=&pass=&domain_id1=100002&edgates1=12615544&squid_select=&squid_edgates=12615544&oe_id=4514232&b_login_id="
-#suchen
-          $cmd="?rm=email_main"
-          $params=""
-
-### main loop ###
-
-$CsvPath = Select-CsvFile
-Validate-CsvFile -file $CsvPath -delimiter ","
-$Import = Import-CSV $CsvPath -Encoding ASCII
-
-foreach ($user in $Import) {
-    $Anrede = $user.Gender
-    $GivenName = [System.Web.HttpUtility]::UrlEncode($user.GivenName)
-    $Surname = [System.Web.HttpUtility]::UrlEncode($user.Surname)
-	$vorname = $user.vorname
-	$nachname = $user.nachname
-
-    $person="&anrede=$Anrede&vname=$GivenName&nname=$Surname&email1=$vorname.$nachname"
-
-    $email= "&stichwort=$vorname.$nachname%40etl.de"
-
-    $date = Get-Date -Format "yyyy-MM-dd_HH:mm:ss"
-
-    try {
-        #create 
-            # $req = "http://$url$script$cmd$params$person"
-        #search #
-            $req = "http://$url$script$cmd$params$email"
-            write-host $req
-         $response = Invoke-WebRequest $req -WebSession $session
-      
-         Add-content $logfile -value $date" Success "$Surname" "$Response
-         Add-content $logfile -value ""
-         if( $Response -match '(.+email_id=)(\d+)(.+)' ) {
-           $id = $Matches[2]
-           $Surname = [System.Web.HttpUtility]::UrlDecode($Surname)
-           $GivenName = [System.Web.HttpUtility]::UrlDecode($GivenName)
-           Write-Host  $GivenName" "$Surname": "$id
-         }else {
-           Write-Host  $GivenName" "$Surname": nicht gefunden"
-         }
-    }
-    catch {
-      Add-content $logfile -value $date" Error "$Surname" "$_.Exception.Message
-      Add-content $logfile -value ""
-    }
-}
-
 function Select-CsvFile {
     
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
@@ -117,8 +52,8 @@ function Validate-CsvFile ($file, $delimiter){
     $read.Dispose()
     
 }
-
 function User-Login {
+  $cookie = New-Object System.Net.Cookie
   $login = read-host -prompt "Enter login"
   $securedValue = Read-Host "Enter password" -AsSecureString
   $sess = New-Object Microsoft.PowerShell.Commands.WebRequestSession
@@ -129,7 +64,6 @@ function User-Login {
   $post = @{rm='form_login';login=$login;kennwort=$psw}
 
 
-  #$post = @{rm='form_login';login='ansa038';kennwort='8533Eg7739!'}
   $res = Invoke-WebRequest "http://$url$script" -Method POST -Body $post -SessionVariable $sess
   #Write-Host $res.RawContent
   if ($res.RawContent -match 'type=password'){
@@ -144,4 +78,128 @@ function User-Login {
              #Write-Host $cookie.Name "=" $cookie.Value
     }
   }
+}
+######################## main ####################
+
+Add-Type -AssemblyName System.Web
+
+$logfile = "create_user_log.log"
+
+$url="edga.ncc.eurodata.de"
+$script="/cgi-bin/index.pl"
+
+if($session -eq $null){ $global:session = New-Object Microsoft.PowerShell.Commands.WebRequestSession }
+if($session.Cookies.Count -eq 0){ User-Login }
+$session2= $session
+### 1. ### 
+# frage nach Email/Suchbegriff
+###
+$suchbegriff = Read-Host -Prompt "Email/Suchbegriff"
+$suchbegriff = [System.Web.HttpUtility]::UrlEncode($suchbegriff)
+### 2. ###
+# liste alle entsprechend dem Suchmuster  gefundenen Email-IDs  
+###
+$request = "http://{0}/cgi-bin/index.pl?rm=email_main&stichwort={1}" -f $url,$suchbegriff
+$response = Invoke-WebRequest $request -WebSession $session
+$pattern  = '(.+email_id=)(\d+)([^>]+>)([^<]+)(.+)'
+$r_emails = Select-String -InputObject $response -Pattern $pattern -AllMatches
+"{0} Email-Adresse(n) gefunden zu diesem Suchbegriff" -f $r_emails.Matches.Count
+Write-Host 'Davon sind folgende Accounts verfügbar:'
+Write-Host '-----------------------------------------'
+#
+# es können mehrere Email eines Account gefunden worden sein - alle durchsuchen
+#
+$n = 1
+$accounts = @()
+foreach ($i in $r_emails.Matches) {
+  $email_id = $i.Groups[2].Value
+  $email    = $i.Groups[4].Value
+
+  ### 3. ###
+  # liste alle gefundenen Account-IDs auf  
+  ###
+  $pattern = '(/show_emails_by_person\.asp\?id=\d*)'
+  $request="http://{0}/show_email.asp?email_id={1}" -f $url,$email_id
+  $response = Invoke-WebRequest $request -WebSession $session
+  $r_accounts = Select-String -InputObject $response -Pattern $pattern -AllMatches
+  
+  #
+  if ($r_accounts.Matches.Count -ne 0) {
+    if( -not $accounts.Contains($r_accounts.Matches.Groups[1].Value))
+    {$accounts+= $r_accounts.Matches.Groups[1].Value
+    #if($n -gt 9) { $s = ''} else {$s = ' '}
+    #"{0}{1}) Account HauptEmailID: {3} # {4}" -f $s, $n, $email_id, $email
+    $n = $n + 1
+    }
+  }
+}  
+$n = $n - 1
+if ($n -eq 0) { Write-Host 'Keine Postfächer gefunden' }
+else {
+  "{0} Accounts gefunden" -f $accounts.Length
+  $pattern = '(.+pf_id.+value=[^\d]+)(\d+)(.+)'
+  foreach( $cmd in $accounts){
+    ### 4. ###
+    # Suche nach den Postfächern  
+    ###
+    $request  ="http://{0}{1}" -f $url,$cmd
+    $response = Invoke-WebRequest $request -WebSession $session
+    $mmm = Select-String -InputObject $response -Pattern $pattern -AllMatches 
+    #"E-Mail-Ad in diesem Account: {0}" -f $mmm.Matches.Count
+    foreach ($iii in $mmm.Matches) {write-host "#########PF IDs : "$iii.Groups[2].Value}
+  }
+}
+
+
+exit
+
+
+#if( $i -match '(.+email_id=)(?<id>\d+)(.+)' ) {  write-host $Matches.id} # else { Write-Host "nix gefunden"}
+# anlegen # $cmd="?rm=insert_person"
+          # $params="&login=&pass=&domain_id1=100002&edgates1=12615544&squid_select=&squid_edgates=12615544&oe_id=4514232&b_login_id="
+#suchen
+          $cmd="?rm=email_main"
+          $params=""
+
+
+$CsvPath = Select-CsvFile
+Validate-CsvFile -file $CsvPath -delimiter ","
+$Import = Import-CSV $CsvPath -Encoding ASCII
+
+foreach ($user in $Import) {
+    $Anrede = $user.Gender
+    $GivenName = [System.Web.HttpUtility]::UrlEncode($user.GivenName)
+    $Surname = [System.Web.HttpUtility]::UrlEncode($user.Surname)
+	$vorname = $user.vorname
+	$nachname = $user.nachname
+
+    $person="&anrede=$Anrede&vname=$GivenName&nname=$Surname&email1=$vorname.$nachname"
+
+    $email= "&stichwort=$vorname.$nachname%40etl.de"
+
+    $date = Get-Date -Format "yyyy-MM-dd_HH:mm:ss"
+
+    try {
+        #create 
+            # $req = "http://$url$script$cmd$params$person"
+        #search #
+            $req = "http://$url$script$cmd$params$email"
+            #write-host $req
+         $response = Invoke-WebRequest $req -WebSession $session
+      
+         Add-content $logfile -value $date" Success "$Surname" "$Response
+         Add-content $logfile -value ""
+         if( $Response -match '(.+email_id=)(\d+)(.+)' ) {
+           $id = $Matches[2]
+           $Surname = [System.Web.HttpUtility]::UrlDecode($Surname)
+           $GivenName = [System.Web.HttpUtility]::UrlDecode($GivenName)
+           Write-Host  $GivenName" "$Surname": "$id
+         }else {
+           Write-Host  $GivenName" "$Surname": nicht gefunden"
+         }
+    }
+    catch {
+      Add-content $logfile -value $date" Error "$Surname" "$_.Exception.Message
+      Add-content $logfile -value ""
+    }
 }
